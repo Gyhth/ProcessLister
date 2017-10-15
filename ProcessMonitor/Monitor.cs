3ec02cea9fc6dd852e2e77119c8b73c56e13c74c
Gyhth <Gyhthy@gmail.com>
2017-10-14 23:01:16 -0400

    Console now loops indefinitely.

---
+++ ProcessMonitor/Monitor.cs
@@ -0,0 +1,42 @@
+﻿using Process_Lister;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace ProcessMonitor
+{
+    class Monitor
+    {
+        public void Start(Writefile writeFile, string binaryPath)
+        {
+            List<SerializedProcess> readProcesses = new List<SerializedProcess>();
+            readProcesses = writeFile.ReadFromBinaryFile<List<SerializedProcess>>(binaryPath);
+            Process[] processlist = Process.GetProcesses();
+            byte[] result;
+            Encryption encryptionManager = new Encryption();
+            StringManipulation stringManipulator = new StringManipulation();
+            foreach (Process process in processlist)
+            {
+                Boolean found = false;
+                result = encryptionManager.generateHash(process.ProcessName);
+                string processName = stringManipulator.ByteArrayToString(result);
+                SerializedProcess serializedProcess = new SerializedProcess();
+                serializedProcess.Name = processName;
+                int index = readProcesses.FindIndex(processList => processList.Name == serializedProcess.Name);
+                if (index > -1)
+                {
+                    found = true;
+                }
+                if (!found)
+                {
+                    process.Kill();
+                }
+            }
+        }
+   
+    }
+}
--- ProcessMonitor/Program.cs
+++ ProcessMonitor/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using Process_Lister;
 using System.Diagnostics;
 using System.IO;
+using System.Timers;
+using System.Threading;
 
 namespace ProcessMonitor
 {
@@ -15,31 +17,14 @@ namespace ProcessMonitor
             StringManipulation stringManip = new StringManipulation();
             if (File.Exists(binaryPath))
             {
-                List<SerializedProcess> readProcesses = new List<SerializedProcess>();
-                readProcesses = writeFile.ReadFromBinaryFile<List<SerializedProcess>>(binaryPath);
-                Process[] processlist = Process.GetProcesses();
-                byte[] result;
-                Encryption encryptionManager = new Encryption();
-                StringManipulation stringManipulator = new StringManipulation();
-                foreach (Process process in processlist)
+                Boolean monitorProcess = true;
+                while (monitorProcess)
                 {
-                    Boolean found = false;
-                    result = encryptionManager.generateHash(process.ProcessName);
-                    string processName = stringManipulator.ByteArrayToString(result);
-                    SerializedProcess serializedProcess = new SerializedProcess();
-                    serializedProcess.Name = processName;
-                    int index = readProcesses.FindIndex(processList => processList.Name == serializedProcess.Name);
-                    if (index > -1) { 
-                        found = true;
-                    }
-                    if (!found)
-                    {
-                        Console.WriteLine(process.ProcessName);
-                        process.Kill();
-                    }
+                    Monitor monitor = new Monitor();
+                    monitor.Start(writeFile, binaryPath);
+                    Thread.Sleep(2000);
                 }
             }
-            Console.ReadLine();
         }
     }
 }
